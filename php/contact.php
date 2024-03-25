b134203a378782878b039672fafb155bda926696
Anas7823 <93275259+Anas7823@users.noreply.github.com>
2024-03-25 15:04:53 +0100

    Update contact.php pwd

--- php/contact.php
+++ php/contact.php
@@ -22,7 +22,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'ThyDev2@gmail.com';                     //SMTP username
-    $mail->Password   = 'IPSSI2019';                               //SMTP password
+    $mail->Password   = 'CODEAGENERE';                               //SMTP password
     $mail->SMTPSecure = "tls"            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
